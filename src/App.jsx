da0ab2bb01722e114fe8aa553c40f7f8db43a45a
Andre Vega <johan.vi.em.99@gmail.com>
2026-06-01 17:12:02 -0500

    se hizo los componentes del login y se dejo listo el login x2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Dashboard from './Dashboard'
-import LoginPage from './components/login/LoginPage'
+import BannerLogin from './components/login/BannerLogin'
+import LoginForm from './components/login/LoginForm'
 import './App.css'
 
 function App() {
@@ -15,9 +16,20 @@ function App() {
   }
 
   return (
-    <main className="app-view">
-      <LoginPage onLogin={() => setCurrentView('dashboard')} />
-    </main>
+    <section className="login-page container-fluid">
+      <div className="login-wrapper">
+        <p className="login-tag mb-2">LOGIN</p>
+
+        <div className="login-card row g-0 overflow-hidden">
+          <div className="col-lg-5">
+            <BannerLogin />
+          </div>
+          <div className="col-lg-7">
+            <LoginForm onLogin={() => setCurrentView('dashboard')} />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/login/BannerLogin.jsx
+++ src/components/login/BannerLogin.jsx
@@ -2,15 +2,15 @@ import stockflowLogo from '../../assets/stockflow.png'
 import './BannerLogin.css'
 
 function BannerLogin() {
-  return (
-    <div className="banner-login-panel">
-      <img
-        src={stockflowLogo}
-        alt="StockFlow"
-        className="img-fluid banner-login-logo"
-      />
-    </div>
-  )
+    return (
+        <div className="banner-login-panel">
+            <img
+                src={stockflowLogo}
+                alt="StockFlow"
+                className="img-fluid banner-login-logo"
+            />
+        </div>
+    )
 }
 
 export default BannerLogin
